80ffeeab6604ac553d00252c0cbba278b21a5be2
Mrbaeksang <qortkdgus95@gmail.com>
2025-09-28 02:23:53 +0900

    Redis 캐시 역직렬화 오류 방지를 위한 CachedPage 타입 정보 추가

--- src/main/kotlin/com/aicounseling/app/global/pagination/CachedPage.kt
+++ src/main/kotlin/com/aicounseling/app/global/pagination/CachedPage.kt
@@ -1,5 +1,6 @@
 package com.aicounseling.app.global.pagination
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
@@ -7,6 +8,11 @@ import org.springframework.data.domain.Pageable
 /**
  * 캐시 계층에서 PageImpl 대신 직렬화하기 쉬운 구조로 사용하기 위한 DTO.
  */
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.CLASS,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "@class",
+)
 data class CachedPage<T>(
     val content: List<T>,
     val totalElements: Long,
