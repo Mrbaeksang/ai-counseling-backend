0b3b9f847bea9c5adc58aa756c125fef924a5758
Mrbaeksang <qortkdgus95@gmail.com>
2025-09-28 11:03:17 +0900

    fix: CachedPage @JsonTypeInfo 어노테이션 제거로 Redis 캐싱 문제 해결
    - @class 속성 강제 추가하는 @JsonTypeInfo 제거
    - LinkedHashMap 캐스팅 에러 근본 해결
    - 불필요한 import문 정리

--- src/main/kotlin/com/aicounseling/app/global/pagination/CachedPage.kt
+++ src/main/kotlin/com/aicounseling/app/global/pagination/CachedPage.kt
@@ -1,6 +1,5 @@
 package com.aicounseling.app.global.pagination
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
@@ -8,11 +7,6 @@ import org.springframework.data.domain.Pageable
 /**
  * 캐시 계층에서 PageImpl 대신 직렬화하기 쉬운 구조로 사용하기 위한 DTO.
  */
-@JsonTypeInfo(
-    use = JsonTypeInfo.Id.CLASS,
-    include = JsonTypeInfo.As.PROPERTY,
-    property = "@class",
-)
 data class CachedPage<T>(
     val content: List<T>,
     val totalElements: Long,
